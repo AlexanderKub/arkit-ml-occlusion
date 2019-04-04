124040b50106588cd71b8f609a075e716274e1c5
Alexander Kub <alexdarkalt@gmail.com>
2019-04-04 17:45:19 +0300

    main functionality

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'arkit-ml-occlusion' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for arkit-ml-occlusion
+  pod 'Fritz'
+  pod 'Fritz/VisionSegmentationModel/People'
+end
--- arkit-ml-occlusion/ViewController.swift
+++ arkit-ml-occlusion/ViewController.swift
@@ -9,16 +9,28 @@
 import UIKit
 import SceneKit
 import ARKit
+import Fritz
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    private lazy var visionModel = FritzVisionPeopleSegmentationModel()
+    
+    var planes = [ARPlaneAnchor: SCNNode]()
+    var planeColor = UIColor.init(hue: 0.5, saturation: 0.5, brightness: 0.5, alpha: 0.5)
+    
+    var maskNode : SCNNode!;
+    var maskMaterial : SCNMaterial!;
+    
+    var currentBuffer: CVPixelBuffer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.session.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
@@ -28,6 +40,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        addTapGestureToSceneView()
+        bilbordCreate()
+        setupUIViews()
+    }
+    
+    func setupUIViews() {
+        let button = UIButton(frame: CGRect(x: 10, y: 10, width: 80, height: 30))
+        button.backgroundColor = .gray
+        button.titleLabel?.font =  UIFont.boldSystemFont(ofSize: 10)
+        button.setTitle("Show mask", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        if (maskMaterial.colorBufferWriteMask == .all) {
+            maskMaterial.colorBufferWriteMask = .alpha
+            sender.setTitle("Show mask", for: .normal)
+            return
+        }
+        maskMaterial.colorBufferWriteMask = .all
+        sender.setTitle("Hide mask", for: .normal)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +71,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.planeDetection = .horizontal
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,17 +82,65 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        guard currentBuffer == nil, case .normal = frame.camera.trackingState else {
+            return
+        }
+        currentBuffer = frame.capturedImage
+        startDetection()
     }
-*/
+    
+    func bilbordCreate() {
+        maskMaterial = SCNMaterial()
+        maskMaterial.diffuse.contents = UIColor.red
+        maskMaterial.colorBufferWriteMask = .alpha
+        
+        let rectangle = SCNPlane(width: 0.0326, height: 0.058)
+        rectangle.materials = [maskMaterial]
+        
+        maskNode = SCNNode(geometry: rectangle)
+        maskNode?.eulerAngles = SCNVector3Make(0, 0, 0)
+        maskNode?.position = SCNVector3Make(0, 0, -0.05)
+        maskNode.renderingOrder = -1
+        
+        sceneView.pointOfView?.presentation.addChildNode(maskNode!)
+    }
+    
+    let visionQueue = DispatchQueue(label: "com.vision.ARML.visionqueue")
+    
+    func pixelBufferToUIImage(pixelBuffer: CVPixelBuffer) -> UIImage {
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        let context = CIContext(options: nil)
+        let cgImage = context.createCGImage(ciImage, from: ciImage.extent)
+        let uiImage = UIImage(cgImage: cgImage!)
+        return uiImage
+    }
+    
+    private func startDetection() {
+        // To avoid force unwrap in VNImageRequestHandler
+        guard let buffer = currentBuffer else { return }
+        
+        let image = FritzVisionImage(image: pixelBufferToUIImage(pixelBuffer: buffer))
+        image.metadata = FritzVisionImageMetadata()
+        let options = FritzVisionSegmentationModelOptions()
+        options.imageCropAndScaleOption = .scaleFit
+        
+        visionQueue.async {
+            // Run our CoreML Request
+            self.visionModel.predict(image, options: options) { [weak self] (mask, error) in
+                guard let mask = mask else { return }
+                let maskImage = mask.toImageMask(of: FritzVisionPeopleClass.person, threshold: 0.70, minThresholdAccepted: 0.30)
+                DispatchQueue.main.async {
+                    self?.maskMaterial.diffuse.contents = maskImage
+                }
+            }
+            
+            // The resulting image (mask) is available as observation.pixelBuffer
+            // Release currentBuffer when finished to allow processing next frame
+            self.currentBuffer = nil
+        }
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -72,4 +156,70 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        let plane = SCNPlane(width: width, height: height)
+        
+        plane.materials.first?.diffuse.contents = planeColor
+        
+        let planeNode = SCNNode(geometry: plane)
+        
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x,y,z)
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        node.addChildNode(planeNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+            else { return }
+        
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+        
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+    }
+    
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.addShipToSceneView(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func addShipToSceneView(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        
+        guard let hitTestResult = hitTestResults.first else { return }
+        
+        let boxGeometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        
+        let translation = SCNVector3(hitTestResult.worldTransform.columns.3.x, hitTestResult.worldTransform.columns.3.y + Float(boxGeometry.height * 0.5), hitTestResult.worldTransform.columns.3.z)
+        let x = translation.x
+        let y = translation.y
+        let z = translation.z
+        
+        let cube = SCNNode(geometry: boxGeometry)
+        
+        cube.position = SCNVector3(x,y,z)
+        sceneView.scene.rootNode.addChildNode(cube)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
 }
